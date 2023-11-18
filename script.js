96361871c60cbff0f43304954f96fcf8ca9b9cbf
Ethanham111 <mtlham45@gmail.com>
2023-11-18 01:01:46 -0500

    Script added, delete unused contents

---
+++ script.js
@@ -0,0 +1,29 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scroll for all links with a hash
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
